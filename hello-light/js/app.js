03a89a56be14bffa90d07272d22a884962b7e032
aniposada <hello@aniposada.com>
2017-06-06 16:02:44 -0700

    Add files via upload

---
+++ hello-light/js/app.js
@@ -0,0 +1,7 @@
+/**
+ * Created by anipo on 2017-04-24.
+ */
+
+particlesJS.load('particles-js', 'assets/particles.json', function() {
+    console.log('callback - particles-js config loaded');
+});
